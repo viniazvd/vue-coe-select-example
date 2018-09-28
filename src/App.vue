9b96d61cf51173354d6385fdb7205e95fcc29cfe
Vinicius Azevedo <viniazvd@gmail.com>
2018-09-28 11:51:12 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id="app" class="container">
+    <c-select
+      label="single"
+      display="slug"
+      display-by="name"
+      placeholder="Selecione uma opção"
+      :clear-on-select="false"
+      :items="items"
+      v-model="data1"
+    />
+
+    <c-select
+      label="single"
+      display="slug"
+      display-by="name"
+      placeholder="Selecione uma opção"
+      clear-on-select
+      :items="items"
+      v-model="data2"
+    >
+      <div slot="option" slot-scope="{ option }">
+        {{ option['slug'] }} - {{ option['name'] }}
+      </div>
+    </c-select>
+
+    <c-select
+      label="multiple"
+      display="slug"
+      display-by="name"
+      placeholder="Selecione uma opção"
+      multiple
+      hide-selected
+      :items="items"
+      v-model="data3"
+    />
+
+    <c-select
+      label="multiple"
+      display="slug"
+      display-by="name"
+      placeholder="Selecione uma opção"
+      :validation="data4.length >= 3 && `Máximo de 3 opções selecionadas`"
+      multiple
+      :items="items"
+      v-model="data4"
+    />
+  </div>
+</template>
+
+<script>
+import CSelect from 'vue-coe-select'
+
+export default {
+  name: 'teste',
+
+  components: { CSelect },
+
+  data () {
+    return {
+      data1: '',
+      data2: '',
+      data3: [
+        { slug: 'slug_boladao2', name: 'coe2' }
+      ],
+      data4: [],
+      items: [
+        { slug: 'slug_boladao1', name: 'coe1' },
+        { slug: 'slug_boladao2', name: 'coe2' },
+        { slug: 'slug_boladao3', name: 'coe3' },
+        { slug: 'slug_boladao4', name: 'coe4' },
+        { slug: 'slug_boladao5', name: 'coe5' },
+        { slug: 'slug_boladao6', name: 'coe6' }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+/* .c-select {
+  position: unset !important;
+  z-index: 1;
+}
+
+.items {
+  z-index: 2;
+  background-color: white;
+} */
+</style>
